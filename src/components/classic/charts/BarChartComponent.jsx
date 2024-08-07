9e4b6a46574ca0893ea4395685170021f47455ed
shah9380 <shahislam9380@gmail.com>
2024-08-07 05:53:03 +0530

    unused removed

--- src/components/classic/charts/BarChartComponent.jsx
+++ src/components/classic/charts/BarChartComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from 'recharts';
 
 // Sample data
